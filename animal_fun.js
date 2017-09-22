c01b2ea67f8aef9f5380744c1c240ad9228b20bc
Howard Feger <hfeger4@gmail.com>
2017-09-21 17:04:11 -0700

    filtered animals by letter

--- animal_fun.js
+++ animal_fun.js
@@ -8,10 +8,29 @@ const fs = require('fs');
 //   }
 // });
 
-fs.writeFile('./example.txt', 'I will be written to example.txt', err =>{
-  if (err){
-    console.log(err);
-  }else{
-    console.log("file successfully written!");
+function selectAnimals(animalString, animalLetter) {
+  return animalString
+    .split('\n')
+    .filter(animal => animal.startsWith(animalLetter))
+    .join('\n')
+}
+
+const animalLetter = process.argv[2].toUpperCase()
+
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err)
+    return
   }
-});
+  const animals = selectAnimals(data, animalLetter)
+
+  fs.writeFile(`${animalLetter}_animals.txt`, animals, err => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    console.log(`successfully created ${animalLetter}_animals.txt`)
+  })
+})
+
+console.log(process.argv[2]);
