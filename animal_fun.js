4e225f441e11f3b753a106341e89fb356ff2d4ee
Howard Feger <hfeger4@gmail.com>
2017-09-22 18:20:44 -0700

    added json file

--- animal_fun.js
+++ animal_fun.js
@@ -12,25 +12,25 @@ function selectAnimals(animalString, animalLetter) {
   return animalString
     .split('\n')
     .filter(animal => animal.startsWith(animalLetter))
-    .join('\n')
+    .join('\n');
 }
 
-const animalLetter = process.argv[2].toUpperCase()
+const animalLetter = process.argv[2].toUpperCase();
 
 fs.readFile('./animals.txt', 'utf-8', (err, data) => {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
-  const animals = selectAnimals(data, animalLetter)
+  const animals = selectAnimals(data, animalLetter);
 
   fs.writeFile(`${animalLetter}_animals.txt`, animals, err => {
     if (err) {
-      console.log(err)
-      return
+      console.log(err);
+      return;
     }
-    console.log(`successfully created ${animalLetter}_animals.txt`)
-  })
-})
+    console.log(`successfully created ${animalLetter}_animals.txt`);
+  });
+});
 
 console.log(process.argv[2]);
